ad44260c8d158594ee415be8f9ab42d1705e5884
FilahAnas <anas.filah@brevo.com>
2024-04-24 11:33:42 +0200

    Run black and remove sort

--- airbyte-integrations/connectors/source-mongodb-python/source_mongodb_python/source.py
+++ airbyte-integrations/connectors/source-mongodb-python/source_mongodb_python/source.py
@@ -25,10 +25,11 @@ from airbyte_cdk.models import (
     StreamDescriptor,
     AirbyteStateBlob,
     AirbyteStreamState,
-    AirbyteStateType
+    AirbyteStateType,
 )
 
-class JsonEncoder():
+
+class JsonEncoder:
     def encode(self, o):
         def handle_object(obj):
             if isinstance(obj, dict):
@@ -39,57 +40,57 @@ class JsonEncoder():
             elif isinstance(obj, ObjectId):
                 return str(obj)
             return obj
+
         return handle_object(o)
 
+
 class SourceMongodbPython(Source):
-    
+
     def check(self, logger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
         try:
             client = self.get_client(logger, config)
-            client.admin.command('ping')
-            logger.info('Successfully connected to MongoDB.')
+            client.admin.command("ping")
+            logger.info("Successfully connected to MongoDB.")
             return AirbyteConnectionStatus(status=Status.SUCCEEDED)
         except Exception as e:
             logger.error(f"Failed to connect to MongoDB: {str(e)}")
-            return AirbyteConnectionStatus(
-                status=Status.FAILED, message=f"Config Check - An exception occurred: {str(e)}"
-            )
-    
+            return AirbyteConnectionStatus(status=Status.FAILED, message=f"Config Check - An exception occurred: {str(e)}")
+
     def get_client(self, logger, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
         """
         Attempts to connect to MongoDB using the provided configuration.
-        
+
         :param logger: Airbyte logger to output logs from this method.
         :param config: A mapping containing the user-provided configuration as specified by the source's spec.yaml
         :return: A tuple of (bool, optional error message). True indicates a successful connection check.
         """
         # Attempt connection using individual parameters
-        verify_mode = config.get('verify_mode', 'true') == 'true'
-        use_ssl = config.get('ssl', 'false') == 'true'
+        verify_mode = config.get("verify_mode", "true") == "true"
+        use_ssl = config.get("ssl", "false") == "true"
 
         connection_params = {
-            "host": config['host'],
-            "username": config.get('user', None),
-            "password": config.get('password', None),
-            "authSource": config['authsource'],
+            "host": config["host"],
+            "username": config.get("user", None),
+            "password": config.get("password", None),
+            "authSource": config["authsource"],
             "ssl": use_ssl,
-            "replicaset": config.get('replica_set', None),
-            "readPreference": 'secondaryPreferred'
+            "replicaset": config.get("replica_set", None),
+            "readPreference": "secondaryPreferred",
         }
-        if config.get('port', None):
-            connection_params["port"]= int(config["port"])
+        if config.get("port", None):
+            connection_params["port"] = int(config["port"])
 
         if not verify_mode and use_ssl:
             connection_params["ssl_cert_reqs"] = ssl.CERT_NONE
 
         client = pymongo.MongoClient(**connection_params)
-        client.admin.command('ping')
+        client.admin.command("ping")
 
         return client
 
     def discover(self, logger, config):
         client = self.get_client(logger, config)
-        db = client[config['database']]
+        db = client[config["database"]]
         streams = []
         for collection_name in db.list_collection_names():
             stream = AirbyteStream(
@@ -104,71 +105,85 @@ class SourceMongodbPython(Source):
         if config.get("schemaless"):
             schema = {"properties": {"data": {"type": "object"}}}
         else:
-            schema = {'properties': {}}
+            schema = {"properties": {}}
             cursor = collection.find({}).limit(10)  # Adjust the limit as necessary
             for doc in cursor:
                 for key in doc.keys():
-                    schema['properties'][key] = {'type': 'string'}
-        schema['properties']["_collection_last_update"] = {'type': 'string'}
-        schema['properties']["_sdc_deleted_at"] = {'type': 'string'}
+                    schema["properties"][key] = {"type": "string"}
+        schema["properties"]["_collection_last_update"] = {"type": "string"}
+        schema["properties"]["_sdc_deleted_at"] = {"type": "string"}
         return schema
-    
+
     def read(self, logger, config, catalog, state):
         client = self.get_client(logger, config)
-        db = client[config['database']]
-        oplog = client['local']['oplog.rs']
-         
+        db = client[config["database"]]
+        oplog = client["local"]["oplog.rs"]
+
         for configured_stream in catalog.streams:
             sync_mode = configured_stream.sync_mode
             stream = configured_stream.stream
             collection_name = stream.name
             collection = db[collection_name]
-            _collection_last_update= int(datetime.now().timestamp()) * 1000
+            _collection_last_update = int(datetime.now().timestamp()) * 1000
             query = {}
 
             if sync_mode == SyncMode.incremental:
                 state_collection_last_update = None
                 for state_message in state:
-                    if state and state_message.stream.stream_descriptor.name == collection_name and state_message.stream.stream_state._collection_last_update:
+                    if (
+                        state
+                        and state_message.stream.stream_descriptor.name == collection_name
+                        and state_message.stream.stream_state._collection_last_update
+                    ):
                         state_collection_last_update = Timestamp(int(state_message.stream.stream_state._collection_last_update), 1)
 
-                start_date = Timestamp(int(datetime.strptime(config['start_date'], "%Y-%m-%dT%H:%M:%S").timestamp()), 1) if config.get('start_date', None) else Timestamp(0,1) 
+                start_date = (
+                    Timestamp(int(datetime.strptime(config["start_date"], "%Y-%m-%dT%H:%M:%S").timestamp()), 1)
+                    if config.get("start_date", None)
+                    else Timestamp(0, 1)
+                )
 
                 filtre = {
-                    'ts': {'$gt': max(state_collection_last_update, start_date) if state_collection_last_update else start_date},
-                    'op': {'$in': ['i', 'u', 'd']},
-                    'ns': f'{config["database"]}.{collection_name}'
+                    "ts": {"$gt": max(state_collection_last_update, start_date) if state_collection_last_update else start_date},
+                    "op": {"$in": ["i", "u", "d"]},
+                    "ns": f'{config["database"]}.{collection_name}',
                 }
-                pipeline = [
+                cursor_pipeline = [
                     {"$match": filtre},
-                    {"$sort": {"ts": -1}},  
-                    {"$group": {
-                        "_id": {
-                            "$cond": {
-                                "if": {"$eq": ["$op", "u"]},
-                                "then": "$o2._id",
-                                "else": "$o._id"
-                            }
-                        },
-                        "recentDate": {"$first": "$ts"},
-                        "operationType": {"$first": "$op"}  # Track the operation type
-                    }},
+                    {
+                        "$group": {
+                            "_id": {"$cond": {"if": {"$eq": ["$op", "u"]}, "then": "$o2._id", "else": "$o._id"}},
+                            "operationType": {"$first": "$op"},
+                        }
+                    },
                 ]
-                distinct_ids_cursor = oplog.aggregate(pipeline)
+                distinct_ids_cursor = oplog.aggregate(cursor_pipeline)
+                recebt_date_pipeline = [
+                    {"$match": filtre},
+                    {
+                        "$group": {
+                            "_id": None,
+                            "recentDate": {"$max": "$ts"},
+                        }
+                    },
+                ]
+                recent_date = datetime.utcfromtimestamp(next(oplog.aggregate(recebt_date_pipeline), None)["recentDate"].time).strftime(
+                    "%Y-%m-%dT%H:%M:%S.%fZ"
+                )
                 deletes_to_process = []
                 distinct_ids_list = []
-                recent_dates = []
 
                 for id_obj in distinct_ids_cursor:
-                    if id_obj["operationType"] == 'd':
-                        deletes_to_process.append({
-                            "_id": str(id_obj["_id"]),
-                            "_sdc_deleted_at": datetime.utcfromtimestamp(id_obj["recentDate"].time).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-                        })
-                    else: 
+                    if id_obj["operationType"] == "d":
+                        deletes_to_process.append(
+                            {
+                                "_id": str(id_obj["_id"]),
+                                "_sdc_deleted_at": recent_date,
+                            }
+                        )
+                    else:
                         if id_obj["_id"] is not None:
                             distinct_ids_list.append(id_obj["_id"])
-                            recent_dates.append(id_obj["recentDate"])
                 logger.info(f"Sync objects for {collection_name} :{len(distinct_ids_list)} with deletes: {len(deletes_to_process)}")
 
                 for delete_doc in deletes_to_process:
@@ -179,16 +194,16 @@ class SourceMongodbPython(Source):
                     )
                     yield AirbyteMessage(type=Type.RECORD, record=record)
 
-                query = {'_id': {'$in': distinct_ids_list}}
-                _collection_last_update = max(recent_dates).time if recent_dates else None
-    
+                query = {"_id": {"$in": distinct_ids_list}}
+                _collection_last_update = recent_date
+
             cursor = collection.find(query)
-            
+
             for doc in cursor:
-                doc['_collection_last_update'] = _collection_last_update
+                doc["_collection_last_update"] = _collection_last_update
                 doc = JsonEncoder().encode(doc)
                 if config.get("schemaless"):
-                    doc = {'data': doc}
+                    doc = {"data": doc}
                 record = AirbyteRecordMessage(
                     stream=collection_name,
                     data=doc,
@@ -207,5 +222,3 @@ class SourceMongodbPython(Source):
                 yield AirbyteMessage(type=Type.STATE, state=stream_state)
 
         client.close()
-
-
